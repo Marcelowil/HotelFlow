5ff8d9d783d66b10660aba72ac988419f8483f3e
Marcelo William <marcelo.willian@live.com>
2026-08-17 16:06:20 -0300

    feat: implementa cadastro de reservas

--- HotelFlow/Application/Services/ReservaService.cs
+++ HotelFlow/Application/Services/ReservaService.cs
@@ -1,11 +1,12 @@
 ﻿using HotelFlow.Domain.Entities;
 using HotelFlow.Domain.Enums;
+using HotelFlow.Domain.Exceptions;
 
 namespace HotelFlow.Application.Services
 {
     public class ReservaService
     {
-        public List<Reserva> reservas = new List<Reserva>();
+        private List<Reserva> reservas = new List<Reserva>();
 
         public bool VerificarDisponibilidade(Quarto quarto, DateTime dataEntrada, DateTime dataSaida)
         {
@@ -13,15 +14,27 @@ namespace HotelFlow.Application.Services
                 .Where(reserva => reserva.Quarto.Numero == quarto.Numero && !new[] { StatusReserva.Cancelada, StatusReserva.Concluida }.Contains(reserva.Status))
                 .ToList();
 
-                foreach (Reserva reserva in reservasPorQuarto)
-                {
-                    bool periodoDisponivel = dataEntrada.Date >= reserva.DataSaida.Date || dataSaida.Date <= reserva.DataEntrada.Date;
+            foreach (Reserva reserva in reservasPorQuarto)
+            {
+                bool periodoDisponivel = dataEntrada.Date >= reserva.DataSaida.Date || dataSaida.Date <= reserva.DataEntrada.Date;
 
-                    if (!periodoDisponivel)
-                        return false;
-                }
+                if (!periodoDisponivel)
+                    return false;
+            }
 
             return true;
+        }
+
+        public Reserva CadastrarReserva(Hospede hospede, Quarto quarto, DateTime dataEntrada, DateTime dataSaida)
+        {
+            Reserva reserva = Reserva.Cadastrar(hospede, quarto, dataEntrada, dataSaida);
+
+            if (!VerificarDisponibilidade(quarto, dataEntrada, dataSaida))
+                throw new ReservaException("O quarto não está disponível para o período informado.");
+
+            reservas.Add(reserva);
+
+            return reserva;
         }
     }
 }
