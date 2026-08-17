a04f87a103c377cdbd02a9e8010d8e842515aa20
Marcelo William <marcelo.willian@live.com>
2026-08-17 15:17:50 -0300

    feat: adiciona servico de disponibilidade de quartos

---
+++ HotelFlow/Application/Services/ReservaService.cs
@@ -0,0 +1,27 @@
+﻿using HotelFlow.Domain.Entities;
+using HotelFlow.Domain.Enums;
+
+namespace HotelFlow.Application.Services
+{
+    public class ReservaService
+    {
+        public List<Reserva> reservas = new List<Reserva>();
+
+        public bool VerificarDisponibilidade(Quarto quarto, DateTime dataEntrada, DateTime dataSaida)
+        {
+            List<Reserva> reservasPorQuarto = reservas
+                .Where(reserva => reserva.Quarto.Numero == quarto.Numero && !new[] { StatusReserva.Cancelada, StatusReserva.Concluida }.Contains(reserva.Status))
+                .ToList();
+
+                foreach (Reserva reserva in reservasPorQuarto)
+                {
+                    bool periodoDisponivel = dataEntrada.Date >= reserva.DataSaida.Date || dataSaida.Date <= reserva.DataEntrada.Date;
+
+                    if (!periodoDisponivel)
+                        return false;
+                }
+
+            return true;
+        }
+    }
+}
